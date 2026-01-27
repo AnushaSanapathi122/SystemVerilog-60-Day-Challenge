95edb14dff3fbb7ba3591d97edb29fbf822d5abf
AnushaSanapathi122 <technocourses122@gmail.com>
2026-01-27 17:14:30 +0530

    Day 1: Basic SystemVerilog hello program

---
+++ day01_introduction/hello_systemverilog.sv
@@ -0,0 +1,9 @@
+// Day 01: Introduction to SystemVerilog
+// Simple hello program
+
+module hello_systemverilog;
+  initial begin
+    $display("Hello SystemVerilog!");
+    $display("Yahoooo!!!!.... Today(27-01-2025) I am Starting 60day system verilog challange");
+  end
+endmodule
